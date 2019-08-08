a7a5345038b95bbbca3393137dcd1402715e21ba
jonas <hallo@grujo.ch>
2019-08-08 18:32:36 +0200

    Little fixes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>PPS</title>
+  <title>Parkleitsystem</title>
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/style.css?v=1.2">
 </head>
@@ -30,7 +30,7 @@
         echo '<div class="pps-main-wrapper">';
           echo '<p class="pps-free ' . $almostFull . '">' . $array['lots'][$i]['free'] . '</p>';
           echo '<p class="pps-total">/ ' . $array['lots'][$i]['total'] . '</p>';
-          echo '<a class="pps-map-icon" href="https://www.google.com/maps/search/Parkhaus+' . $array['lots'][$i]['name'] . '+Basel" target="_blank"><img src="img/map.svg"></a>';
+          echo '<a class="pps-map-icon" href="https://www.google.com/maps/search/Parkhaus+' . $array['lots'][$i]['name'] . '+Basel" target="_blank"><img src="img/map.svg" alt="Google Maps-Suche nach Parkhaus ' . $array['lots'][$i]['name'] . '"></a>';
         echo '</div>';
       echo '</article>';
       $i++;
@@ -47,9 +47,9 @@
     $date = $array['last_updated'];
     $fixedDate = date('H:i, d.m.Y', strtotime($date) + 60*60 );
   ?>
-  <p><a href="."><img src="img/refresh.svg"></a> <?php echo $fixedDate; ?></p>
-	<p><a href="https://api.parkendd.de/Basel" target="_blank">API</a> &middot; <a href="https://parkendd.de/map.html#Basel" target="_blank">Karte</a></p>
+  <p><a href="."><img src="img/refresh.svg" alt="Seite neu laden"></a> <?php echo $fixedDate; ?></p>
+  <!-- <p><a href="https://api.parkendd.de/Basel" target="_blank">API</a> &middot; <a href="https://parkendd.de/map.html#Basel" target="_blank">Karte</a></p> -->
 </footer>
-	
+  
 </body>
 </html>
