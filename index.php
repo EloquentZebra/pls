d84aa255ce51c75cbee4b794382010b738244c40
Jonas <git@grujo.ch>
2022-06-06 09:47:40 +0200

    Add City of Zurich

--- index.php
+++ index.php
@@ -13,15 +13,37 @@
   <section class="container">
 
   <?php
-    $json = file_get_contents("https://api.parkendd.de/Basel");
+
+    if(isset($_GET['city'])) {
+      $choosenCity = htmlspecialchars($_GET["city"]);
+      if ($choosenCity == "Zürich") {
+        $apiUrl = "https://api.parkendd.de/Zuerich";
+      } elseif ($choosenCity == "Basel") {
+        $apiUrl = "https://api.parkendd.de/Basel";
+      } else {
+        $apiUrl = "https://api.parkendd.de/Basel";
+      }
+    } else {
+      $apiUrl = "https://api.parkendd.de/Basel";
+    }
+    
+    
+
+    $json = file_get_contents($apiUrl);
     $array = json_decode($json, true);
     
     $arrayLength = count($array['lots']);
 
     $i = 0;
     while ($i < $arrayLength) {
+      $totalLots = $array['lots'][$i]['total'];
+      $freeLots = $array['lots'][$i]['free'];
 
-      if ($array['lots'][$i]['free'] / $array['lots'][$i]['total'] < 0.15) {
+      if ($totalLots == 0) {
+        $totalLots = 1;
+      }
+
+      if ($freeLots / $totalLots < 0.15) {
         $almostFull = 'pls-danger';
       } else {
         $almostFull = '';
@@ -30,8 +52,8 @@
       echo '<article class="pls-box">';
         echo '<header class="pls-header-wrapper"><h1>' . $array['lots'][$i]['name'] . '</h1><div class="pls-header-links"><a class="pls-icon pls-directions-icon" href="https://www.google.com/maps/dir//' . $array['lots'][$i]['lot_type'] . ' ' . $array['lots'][$i]['name'] . '+Basel" target="_blank"><img src="img/directions.svg" alt="Google Maps-Navigation nach Parkhaus ' . $array['lots'][$i]['name'] . ' aufrufen">   <a class="pls-icon pls-map-icon" href="https://www.google.com/maps/search/' . $array['lots'][$i]['lot_type'] . ' ' . $array['lots'][$i]['name'] . '+Basel" target="_blank"><img src="img/map.svg" alt="Google Maps-Suche nach Parkhaus ' . $array['lots'][$i]['name'] . '"></a></div></header>';
         echo '<main class="pls-main-wrapper">';
-          echo '<p class="pls-free ' . $almostFull . '">' . $array['lots'][$i]['free'] . '</p>';
-          echo '<p class="pls-total">/ ' . $array['lots'][$i]['total'] . '</p>';
+          echo '<p class="pls-free ' . $almostFull . '">' . $freeLots . '</p>';
+          echo '<p class="pls-total">/ ' . $totalLots . '</p>';
         echo '</main>';
       echo '</article>';
       $i++;  
@@ -42,6 +64,8 @@
 </section>
 
 <footer class="text-center text-small">
+  <a href="index.php?city=Basel"><button>Basel</button></a>
+  <a href="index.php?city=Zürich"><button>Zürich</button></a>
   <?php
     $date = $array['last_updated'];
     $fixedDate = date('H:i, d.m.Y', strtotime($date) + 60*60 );
